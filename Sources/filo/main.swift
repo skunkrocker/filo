fc899729d63e16de369190bc88d0244d9809b832
skunkrocker <skunkrocker@gmail.com>
2022-01-27 21:33:50 +0100

    structure the the overview text

--- Sources/filo/main.swift
+++ Sources/filo/main.swift
@@ -3,7 +3,10 @@ import ArgumentParser
 
 struct Filo: ParsableCommand {
     static let configuration = CommandConfiguration(
-        abstract: " Organize your photos with love.\n".yellow.onLightBlue + "Import images from different sources to designated library folder structured by year, month and day.".blue,
+        abstract: " Organize your photos with love.\n".yellow.onLightBlue +
+        " Import images from a source folder to destination library folder.\n".green +
+        " The images are structured in sub folders by year, month and day.\n".yellow +
+        " The dates are taken from the EXIF date stored in the image it self.\n".red,
         subcommands: [Import.self])
     
     init() { }
