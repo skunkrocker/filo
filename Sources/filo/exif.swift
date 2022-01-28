48bf294bcb44aea711c660f45dae301c6b090a8e
Nikola Spasikov <n.spasikov@gmail.com>
2022-01-28 23:02:26 +0100

    print exif data for a photo on the path

--- Sources/filo/exif.swift
+++ Sources/filo/exif.swift
@@ -1,25 +1,45 @@
-
+import TextTable
 import SwiftExif
 import Foundation
 import ArgumentParser
+
+struct EXIF {
+    let key: Any
+    let value: Any
+}
+
+let table = TextTable<EXIF> {
+    [
+        Column(title: "", value: $0.key),
+        Column(title: "", value: $0.value)
+    ]
+}
 
 
 struct Exif: ParsableCommand {
     
     public static let configuration = CommandConfiguration(
-        abstract: "Imports the images from the source folder in to the library folder."
+        abstract: " Reads the EXIF Data of a single image and prints it on the CLI.".blue
     )
     
+    @Argument(help: "The absolute path of the photo for which the EXIF should be read")
+    private var path: String
+    
     func run() throws {
-        let testFilePath = "/Users/nik/projects/cli/filo/Tests/20211230_151252.jpg"
-        let url = URL(fileURLWithPath: testFilePath)
+        //let testFilePath = "/Users/nik/projects/cli/filo/Tests/20211230_151252.jpg"
+        let url = URL(fileURLWithPath: path)
         let exifImage = SwiftExif.Image(imagePath: url)
+        
+        var data: [EXIF] = []
         
         for dc in exifImage.Exif() {
             for meta in dc.value {
-                print("\(meta.key)->\(meta.value)")
+                //print("\(meta.key)->\(meta.value)")
+                data.append(EXIF(key: meta.key, value: meta.value))
             }
         }
+        
+        table.print(data, style: Style.plain)
         
     }
     
