03bc2f04772901f3b307d596f62681288b678ff5
skunkrocker <skunkrocker@gmail.com>
2022-01-30 16:18:53 +0100

    refactor display of cmds, db add and commit library config

--- Sources/filo/main.swift
+++ Sources/filo/main.swift
@@ -4,12 +4,12 @@ import ArgumentParser
 
 struct Filo: ParsableCommand {
     static let configuration = CommandConfiguration(
-        abstract:
-        "\n ❤️  Organize your photos with love ❤️\n".red +
-        " " + TRAFIC_LIGHT + " Import photos from a source folder to destination library folder\n".blue +
-        " " + TRAFIC_LIGHT + " The photos are structured in sub folders by year, month and day\n".blue +
-        " " + TRAFIC_LIGHT + " The dates are taken from the EXIF date stored in the photo it self\n".blue,
-        subcommands: [Import.self, Exif.self])
+        abstract: NL +
+        "        ❤️  Organize your photos with love ❤️\n".red +
+        "        " + TRAFIC_LIGHT + " Import photos from a source folder to destination library folder.\n".blue +
+        "        " + TRAFIC_LIGHT + " The photos are structured in sub folders by year, month and day.\n".blue +
+        "        " + TRAFIC_LIGHT + " The dates are taken from the EXIF date stored in the photo it self.\n".blue,
+        subcommands: [Config.self, Import.self, Exif.self])
     
     init() { }
 }
