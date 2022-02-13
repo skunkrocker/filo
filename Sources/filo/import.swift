f329985233efcecb1ea43ca02d1bb6834bee8e12
skunkrocker <skunkrocker@gmail.com>
2022-02-13 14:32:12 +0100

    import the images in the right command

--- Sources/filo/import.swift
+++ Sources/filo/import.swift
@@ -3,21 +3,9 @@ import ArgumentParser
 
 struct Import: ParsableCommand{
     
-    public static let configuration = conf("Import photos from the source to the library folder.")
-    
-    @Option(name: .shortAndLong, help: help_blue("Path to the folder with the images for importing."))
-    private var src: String?
-    
-    
-    @Option(name: .shortAndLong, help: help_blue("Path to the library folder where the images are imported."))
-    private var lib: String?
-    
-    
+    public static let configuration = conf("Import media from the source to the library folder.")
+
     func run() throws {
-        if src == nil || lib == nil {
-            print("The source and library are mandatory".red)
-            return
-        }
-        print("Source folder: \(src!) -> library: \(lib!)")
+       forAllSrcAndLibs()
     }
 }
